a519eeeaaea4ccaeb60772bb898fab84f4edb739
arviyonismara <arviyonismara@gmail.com>
2022-09-17 19:26:10 +0700

    manitpulasi DOM

---
+++ belajar-javascript/DOM/manipulation/script.js
@@ -0,0 +1,41 @@
+/*DOM Manipulation*/
+
+
+// 1. innerHTML = untuk merubah isi dalam sebuah elemen / tag
+// const judul = document.getElementById('judul')
+// judul.innerHTML = "Arvie Arvearie Yonismara"
+
+// const sectionA = document.querySelector('section#a')
+// sectionA.innerHTML = 'Hello World'
+
+// 2. style. = untuk merubah style sebuah elemen
+// const judul = document.querySelector('#judul')
+// judul.style.backgroundColor = 'lightblue'
+
+// 3. .setAttribut = untuk menambahkan attribut baru kedalam sebuah elemen
+// const judul = document.getElementsByTagName('h1')[0];
+// judul.setAttribute('name', 'Arvie')
+
+// const a = document.querySelector('section#a a')
+// a.setAttribute('id', 'link')
+
+// 4. getAttribut = untuk menampilkan attribut apa yang dimiliki sebuah elemen
+// a.getAttribut('href')
+
+// 5. removeAttribut = untuk menghapus sebuah attribut dalam sebuah elemen
+// a.removeAttribut('id')
+
+// 6. .classList = untuk menampilkan class2
+const p2 = document.querySelector('.p2');
+
+// 7. .classList.add = untu menambahkan class ke sebuah elemen
+
+// 8. .classList.remove = untuk menghapus class
+
+// 9. .classList.toggle = untuk menambah class jika belum ada class yang ditulis, menghapus class jika terdapat class yang ditulis
+
+// 10. .classList.item = untuk mencari class sesuai urutan index
+
+// 11. .classList.contains = untuk mencari sebuah class didalam sebuah elemen
+
+// 12. .classList.replace = untuk mengganti class dalam elemen
