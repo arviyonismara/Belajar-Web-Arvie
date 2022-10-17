5161be0803d77d389da2721673a8adfceb550c4a
arviyonismara <arviyonismara@gmail.com>
2022-10-17 12:28:56 +0700

    web-worker

--- belajar-javascript/javascript lanjutan/asynchronous/web-worker.js
+++ belajar-javascript/javascript lanjutan/asynchronous/web-worker.js
@@ -1,13 +1,11 @@
-const worker = new Worker("web-worker.js")
-
 function showLog(total){
     for(let i = 0; i < total; i++){
         console.log(i)
     }
 }
 
-function buttonClick(){
-    console.log("start Log");
-    showLog(20000);
-    console.log("Finish Log")
-}
+addEventListener("message", function(event){
+    const total = event.data;
+    showLog(total);
+    postMessage("Done")
+})
